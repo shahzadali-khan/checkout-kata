aa51ac5f3546ea6b9db3b57b8d721a509b331e54
shahzadali-khan <shahzadalikhan.iiui@gmail.com>
2023-06-23 19:19:03 +0500

    init implementation

---
+++ Kata.Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Checkout.Tests
+{
+
+    [TestFixture]
+    public class CheckoutTests
+    {
+        private Checkout checkout;
+
+        [SetUp]
+        public void Setup()
+        {
+
+
+            checkout = new Checkout();
+        }
+
+        [Test]
+        public void Scan_ValidItem_AddsItemToCart()
+        {
+            // Arrange
+            char item = 'A';
+
+            // Act
+            checkout.Scan(item);
+
+            // Assert
+            Assert.AreEqual(1, checkout.GetItemCount(item));
+        }
+
+        [Test]
+        public void GetTotalPrice_NoItemsInCart_ReturnsZero()
+        {
+            // Arrange
+
+            // Act
+            int totalPrice = checkout.GetTotalPrice();
+
+            // Assert
+            Assert.AreEqual(0, totalPrice);
+        }
+
+        [Test]
+        public void GetTotalPrice_ItemsWithNoOffers_CalculatesTotalPrice()
+        {
+            // Arrange
+            checkout.Scan('A');
+            checkout.Scan('B');
+            checkout.Scan('C');
+
+            // Act
+            int totalPrice = checkout.GetTotalPrice();
+
+            // Assert
+            Assert.AreEqual(100, totalPrice);
+        }
+
+        [Test]
+        public void GetTotalPrice_ItemsWithOffers_CalculatesTotalPrice()
+        {
+            // Arrange
+            checkout.Scan('A');
+            checkout.Scan('A');
+            checkout.Scan('A');
+            checkout.Scan('B');
+            checkout.Scan('B');
+            checkout.Scan('C');
+            // Act
+            int totalPrice = checkout.GetTotalPrice();
+
+            // Assert
+            Assert.AreEqual(6, checkout.GetCartItemCount());
+            Assert.AreEqual(215, totalPrice);
+        }
+    }
+
+}
---
+++ Kata.Checkout/Checkout.cs
@@ -0,0 +1,17 @@
+﻿using Kata.Checkout.Interfaces;
+
+namespace Kata.Checkout
+{
+    public class Checkout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(char item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
